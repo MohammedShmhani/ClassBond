f8dce93af7877b0bca7132e285db948552f08397
Tornado515 <113722578+Tornado515@users.noreply.github.com>
2025-10-09 22:14:38 +0300

    fixed package error

--- src/main/java/com/example/classbond/TestController.java
+++ src/main/java/com/example/classbond/TestController.java
@@ -1,4 +1,4 @@
-package com.example.classbond.controller;
+package com.example.classbond;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
